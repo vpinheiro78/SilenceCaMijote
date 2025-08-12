34ebe6811d8a9731f5fbb1961ba996f2902bc89e
vpinheiro78 <victor_pinheiro6@hotmail.com>
2025-08-12 15:51:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
-export const SUPABASE_URL = "https://ttkgzzamsfnittbqqvft.supabase.co";
-export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0a2d6emFtc2ZuaXR0YnFxdmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4Mjg4MTEsImV4cCI6MjA3MDQwNDgxMX0.aE5GxrKrNJoqr1g8ASVG9Vdf7k_OLuyCOe2vZAp0-wY";
+export const SUPABASE_URL = 'https://ttkgzzamsfnittbqqvft.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0a2d6emFtc2ZuaXR0YnFxdmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4Mjg4MTEsImV4cCI6MjA3MDQwNDgxMX0.aE5GxrKrNJoqr1g8ASVG9Vdf7k_OLuyCOe2vZAp0-wY';
 export const BUCKET_NAME = 'photos-recettes'
